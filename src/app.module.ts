f9cb83d23e11e04196dcf7b259b6f663257ab0c8
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2023-06-28 16:30:11 +0300

    feat: set up server for static resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AlbumModule } from './albums/album.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use('/upload', express.static(join(__dirname, '..', 'upload')));
+
   await app.listen(3000);
 }
 bootstrap();
